39e9c16ac1fdb7cbfa36ef35227fdc99da1c5c05
Aishah <maishah2540@gmail.com>
2024-04-22 12:01:55 +0300

    Enable buttons in login page to redirect to necessary pages.

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 '''
 Contains functionalities for authorization pages (login, signup, logout).
 '''
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     '''
     Defines view functionality for the login page.
 
     Returns: The login page.
     '''
+    if request.method == 'POST':
+        email = request.form.get('emmail')
+        password = request.form.get('password')
+
+        print(email, password)
+        # Check user exists in database.
+        # Check password is correct.
+        # redirect to homepage.
+        return redirect(url_for('views.home'))
+        # Login user.
+        # If password is not correct, flash error
+        # If user does not exist flash error.
     return render_template("login.html")
 
 @auth.route('/logout')
