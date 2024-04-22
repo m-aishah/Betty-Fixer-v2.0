b569ed56292c20d431b6d57941691df539a0d788
Aishah <maishah2540@gmail.com>
2024-04-22 16:38:14 +0300

    Implement some functionality in login page. Some safety checks & buttons

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 Contains functionalities for authorization pages (login, signup, logout).
 '''
 
+# NEED TO IMPLEMENT CONTINUE WITH GOOGLE BUTTON! LOOK UP HOW TO DO IT!!!
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -13,7 +15,7 @@ def login():
     Returns: The login page.
     '''
     if request.method == 'POST':
-        email = request.form.get('emmail')
+        email = request.form.get('email')
         password = request.form.get('password')
 
         print(email, password)
@@ -35,12 +37,32 @@ def logout():
     '''
     return "<h1>Logout</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     '''
     Defines view functionality for the sign up page.
     
     Returns: The sign up page.
     '''
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email is too short. Please enter a valid email', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')  
+        else:
+            # Create new user
+            # Add new user to the database
+            # LOgin user
+            # Print/Flash success message.
+            flash('Account created', category='success')
+            # Redirect to homepage.
+            return redirect(url_for('views.home'))
+        
     return render_template("sign_up.html")
 
